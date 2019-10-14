56b31a6efd1b72410fb613c3198d0f9bf5abb66d
Steven <steventruong1995@yahoo.com>
2019-10-14 17:46:06 -0400

    add home view

---
+++ BlogMastery.Tests/HomeControllerTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BlogMastery.Tests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
