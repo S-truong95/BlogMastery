643af3176044f3318e7944ccad823516903cda05
mmosier-web <mmosier5@gmail.com>
2019-10-15 18:10:32 -0400

    added post index view

--- BlogMastery/Controllers/PostController.cs
+++ BlogMastery/Controllers/PostController.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogMastery.Models;
+using BlogMastery.Repository;
 
 namespace BlogMastery.Controllers
 {
     public class PostController
     {
+        IRepository<Post> postRepo;
+
+        public PostController(IRepository<Post> postRepo)
+        {
+            this.postRepo = postRepo;
+
+        }
+
+        //public ViewResult PostIndex()
+        //{
+        //    var model = postRepo.GetAll();
+        //    return View(model);
+        //}
+
 
     }
 }
--- BlogMastery/Repository/PostRepository.cs
+++ BlogMastery/Repository/PostRepository.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogMastery.Data;
 using BlogMastery.Models;
 
 namespace BlogMastery.Repository
 {
     public class PostRepository : IRepository<Post>
     {
+        private Context db;
+
         public IEnumerable<Post> GetAll()
         {
-            throw new NotImplementedException();
+            return db.Posts.ToList();
         }
 
         public Post GetByID(int id)
         {
-            throw new NotImplementedException();
+            return db.Posts.Single(p => p.Id == id);
         }
 
         public IEnumerable<Post> GetByProductID(int id)
