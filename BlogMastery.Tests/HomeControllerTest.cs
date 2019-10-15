c428acb8c593fbc938afe4eddb507a92ce34e549
mmosier-web <mmosier5@gmail.com>
2019-10-15 15:54:15 -0400

    added test

--- BlogMastery.Tests/HomeControllerTest.cs
+++ BlogMastery.Tests/HomeControllerTest.cs
@@ -1,14 +1,21 @@
 using System;
+using BlogMastery.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using Xunit;
+
 
 namespace BlogMastery.Tests
 {
     public class HomeControllerTest
     {
         [Fact]
-        public void Test1()
+        public void Index_Returns_View()
         {
+            var controller = new HomeController();
 
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
